ff8e23424c4124dbc1a139aabbabb9fe2d228432
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-10-03 20:47:02 -0400

    feat: Carrosel incompleto

---
+++ src/screens/Carrousel.tsx
@@ -0,0 +1,161 @@
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  SafeAreaView,
+  Image,
+  FlatList,
+} from 'react-native';
+
+const ListItem = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <Image
+        source={{
+          uri: item.uri,
+        }}
+        style={styles.itemPhoto}
+        resizeMode="cover"
+      />
+      <Text style={styles.itemText}>{item.text}</Text>
+    </View>
+  );
+};
+
+export const Carrousel = () => {
+  return (
+    <FlatList
+    horizontal
+    data={SECTIONS[0].data}
+    renderItem={({ item }) => <ListItem item={item} />}
+    showsHorizontalScrollIndicator={false}
+  />
+);
+};
+const SECTIONS = [
+  {
+    title: 'Made for you',
+    horizontal: true,
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/10/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1002/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1006/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1008/200',
+      },
+    ],
+  },
+
+  
+  {
+    title: 'Punk and hardcore',
+    data: [
+      {
+        key: '1',
+        text: 'Item text 12',
+        uri: 'https://picsum.photos/id/1011/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/1012/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1013/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1015/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1016/200',
+      },
+    ],
+  },
+  {
+    title: 'Based on your recent listening',
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1020/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/1024/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1027/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1035/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1038/200',
+      },
+    ],
+  },
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+  sectionHeader: {
+    fontWeight: '800',
+    fontSize: 18,
+    color: '#f4f4f4',
+    marginTop: 20,
+    marginBottom: 5,
+  },
+  item: {
+    margin: 10,
+  },
+  itemPhoto: {
+    width: 200,
+    height: 200,
+  },
+  itemText: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    marginTop: 5,
+  },
+});
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,23 @@
 import { View, Text, TextInput, Image, SafeAreaView } from "react-native";
+import { Carrousel } from "./Carrousel";
 
 const Home = () => {
   return (
     <SafeAreaView className="flex-1 justify-center">
       <View className="flex-1 bg-gray-800">
-        <Text className="text-zinc-400 text-lg ml-6 mt-10 font-semibold">
+        <Text className="text-zinc-400 text-lg ml-6 mt-10 font-extrabold">
           O que gostaria de ler?
         </Text>
         <TextInput
-          className="h-10 m-3 p-3 bg-zinc-700 rounded-2xl text-neutral-500 text-sm font-normal"
+          className="h-10 m-4 p-3 bg-zinc-700 rounded-2xl text-white text-sm font-normal"
           placeholder="Pesquisar"
         />
-        <View className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          {/* <Image
-          source={require("./caminho-para-sua-imagem.png")}
-          className="w-6 h-6 text-gray-400"
-        /> */}
-        </View>
-        <Text className="text-slate-300">Destaques</Text>
+
+
+      </View>
+
+      <View className="absolute inset-y-0 top-32 flex items-center">
+        <Carrousel />
       </View>
     </SafeAreaView>
   );
