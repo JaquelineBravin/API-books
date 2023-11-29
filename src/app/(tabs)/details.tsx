1cc1a92e0e81ec35f589b81d40afac9916be9d9e
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-11-29 09:30:53 -0400

    feat: passing information from the home screen to the Details screen

--- src/app/(tabs)/details.tsx
+++ src/app/(tabs)/details.tsx
@@ -1,40 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import {
-	getCurrentPositionAsync,
-	requestForegroundPermissionsAsync,
-	LocationObject
-} from 'expo-location';
+import { useLocalSearchParams } from 'expo-router';
+// import {
+// 	getCurrentPositionAsync,
+// 	requestForegroundPermissionsAsync,
+// 	LocationObject,
+
+// } from 'expo-location';
+// import { useNavigation } from '@react-navigation/native';
 
 const DetailsScreen: React.FC = () => {
+	const params = useLocalSearchParams();
+	// const navigation = useNavigation();
+
 	// Geolocalização
 	// Pesquise pela latitude e longitude no Google Maps para confirmar a localização do usuário
 	// Exemplo: 37.4226711, -122.0849872
-	const [location, setLocation] = useState<LocationObject | null>(null);
 
-	async function requestLocationPermissions() {
-		const { granted } = await requestForegroundPermissionsAsync();
+	// const [location, setLocation] = useState<LocationObject | null>(null);
 
-		if (granted) {
-			const currentPosition = await getCurrentPositionAsync();
-			setLocation(currentPosition);
+	// async function requestLocationPermissions() {
+	// 	const { granted } = await requestForegroundPermissionsAsync();
 
-			console.log('LOCALIZAÇÃO ATUAL => ', currentPosition);
-		}
-	}
+	// 	if (granted) {
+	// 		const currentPosition = await getCurrentPositionAsync();
+	// 		setLocation(currentPosition);
 
-	useEffect(() => {
-		requestLocationPermissions();
-	}, []);
+	// 		// console.log('LOCALIZAÇÃO ATUAL => ', currentPosition.coords);
+	// 		const locatraduct = currentPosition.coords;
+	// 		console.log('LOCALIZAÇÃO ATUAL => ', locatraduct);
+	// 	}
+	// }
+
+	// useEffect(() => {
+	// 	requestLocationPermissions();
+	// }, []);
 
 	return (
 		<View className="flex-1 items-center bg-gray-800">
 			<Text className="text-lg text-white">Detalhes</Text>
 			<View>
 				<Text className="text-sm">Detalhes</Text>
+				<Text>Id: {params.id}</Text>
+				<Text>Title: {params.title}</Text>
+				<Text>linguagem: {params.laguage}</Text>
+				<Text>Numero de Downloads: {params.NmrDown}</Text>
+				{/* <Text className='text-white text-2xl'>localização: {location?.coords.altitude}</Text> */}
 			</View>
 		</View>
 	);
 };
 
 export default DetailsScreen;
+
+// /*
+// {"accuracy":,
+// "altitude": ,
+//  "altitudeAccuracy":,
+//  "heading": ,
+//  "latitude": ,
+//  "longitude":,
+//  "speed": }*/
--- src/app/components/FlatListBookVertical3Col.tsx
+++ src/app/components/FlatListBookVertical3Col.tsx
@@ -1,31 +1,83 @@
 import { useEffect, useState } from 'react';
-import { FlatList, Image } from 'react-native';
+import { Button, FlatList, Image, Pressable, SectionList, Text } from 'react-native';
 import { Book, fetchBooks } from '../../api/route';
+import { Link, Tabs, useRouter } from 'expo-router';
 
 export const FlatListBookVertical3Col = () => {
+	const route = useRouter();
 	const [books, setBooks] = useState<Book[]>([]);
+
 	useEffect(() => {
 		async function fetchData() {
 			const fetchedBooks: any = await fetchBooks();
 			setBooks(fetchedBooks);
 		}
-
 		fetchData();
 	}, []);
+
+
+	const handleImagePress = (book: Book) => {
+		// console.log('book => ', book);
+		route.push({ pathname: "/details", params: { id: book.id, title: book.title, NmrDown: book.download_count, laguage: book.languages } })
+	};
+
+	const DATA = [
+		{
+			title: 'Populares',
+			data: books
+		},
+		// {
+		//   title: 'Em breve',
+		//   data: [books],
+		// },
+		// {
+		//   title: 'Premiados',
+		//   data: [books],
+		// },
+		// {
+		//   title: 'Brasileiros',
+		//   data: [books],
+		// },
+	];
+
 	return (
-		<FlatList
-			horizontal={false}
-			showsHorizontalScrollIndicator={false}
-			numColumns={3}
-			data={books}
-			renderItem={({ item }) => (
-				<Image
-					source={{ uri: item.formats['image/jpeg'] }}
-					className="w-28 h-36 m-2 rounded-2xl top-1"
+		<SectionList
+			sections={DATA}
+			keyExtractor={(item: any, index: any) => item + index}
+			renderSectionHeader={({ section: { title } }) => (
+				<Text className="flex text-white text-base">{title}</Text>
+			)}
+			renderItem={() => (
+				<FlatList
+					horizontal={false}
+					showsHorizontalScrollIndicator={false}
+					numColumns={3}
+					data={books}
+					renderItem={({ item }): any => {
+						return (
+							<Pressable onPress={() => {
+								handleImagePress(item);
+							}}>
+								<Image
+									source={{ uri: item.formats['image/jpeg'] }}
+									className="flex w-36 h-48 m-2 rounded-2xl top-1  "
+								/>
+							</Pressable>
+						);
+					}}
 				/>
 			)}
 		/>
 	);
 };
 
+
+
+
 export default FlatListBookVertical3Col;
+
+
+/*
+download_count
+"languages":
+*/
