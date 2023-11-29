e7f2254203079fd05f3f7274e5675d1afa0680fe
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-11-29 17:50:33 -0400

    feat: add Pressable for FlatListBookHorizontal

--- src/app/components/FlatListBookHorizontal.tsx
+++ src/app/components/FlatListBookHorizontal.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import { FlatList, Image } from 'react-native';
+import { FlatList, Image, Pressable } from 'react-native';
 import { Book, fetchBooks } from '../../api/route';
+import { useRouter } from 'expo-router';
 
 export const FlatListBookHorizontal = () => {
 	const [books, setBooks] = useState<Book[]>([]);
+	const route = useRouter();
 	useEffect(() => {
 		async function fetchData() {
 			const fetchedBooks: any = await fetchBooks();
@@ -12,16 +14,25 @@ export const FlatListBookHorizontal = () => {
 
 		fetchData();
 	}, []);
+
+	const handleImagePress = (book: Book) => {
+		// console.log('book => ', book);
+		route.push({ pathname: "/details", params: { id: book.id, title: book.title, NmrDown: book.download_count, laguage: book.languages, format: book.formats['image/jpeg'] } })
+	};
 	return (
 		<FlatList
 			horizontal={true}
 			showsHorizontalScrollIndicator={false}
 			data={books}
 			renderItem={({ item }) => (
-				<Image
-					source={{ uri: item.formats['image/jpeg'] }}
-					className="w-40 h-56 m-2 rounded-2xl top-1"
-				/>
+				<Pressable onPress={() => {
+					handleImagePress(item);
+				}}>
+					<Image
+						source={{ uri: item.formats['image/jpeg'] }}
+						className="w-40 h-56 m-2 rounded-2xl top-1"
+					/>
+				</Pressable>
 			)}
 		/>
 	);
